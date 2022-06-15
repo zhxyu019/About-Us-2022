b5dc585b9e85c6d745740101a136eaac5bfbdba3
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-06-15 14:50:01 +0800

    Initial Commit

---
+++ About Us/About_UsApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_UsApp.swift
+//  About Us
+//
+//  Created by Jia Chen Yee on 15/6/22.
+//
+
+import SwiftUI
+
+@main
+struct About_UsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ About Us/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  About Us
+//
+//  Created by Jia Chen Yee on 15/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

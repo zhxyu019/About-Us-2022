5f1190cb2fc76646a03855ed8782057bfa9eabde
ReyRachel8L <rlim.rachel08@gmail.com>
2022-06-18 13:20:52 +0800

    updated

--- About Us/Stevens/Rachel.swift
+++ About Us/Stevens/Rachel.swift
@@ -7,14 +7,65 @@
 
 import SwiftUI
 
-struct Rachel: View {
+struct ContentView: View {
+    
+    @State var nameRotation: Angle = .zero
+    @State var showName = false
+    @State var showText = false
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            LinearGradient(colors: [.cyan, .pink, .yellow, .teal],
+                           startPoint: .top,
+                           endPoint: .bottom)
+            
+            VStack {
+                
+                Button {
+                    withAnimation() {
+                        showName = true
+                    }
+                    } label : {
+                  Text("Rachel")
+                    .padding()
+                    .font(.system(size: 50, weight: .heavy))
+                    .onAppear {
+                        withAnimation() {
+                            nameRotation = .degrees(1440)
+                        }
+                    }
+                
+                Image("RL Icon 1")
+                    .resizable()
+                    .scaledToFit()
+                
+                Button {
+                    withAnimation() {
+                        showText = true
+                    }
+                    } label : {
+                        Text("Click to see text")
+                            .padding(10)
+                            .font(.system(size: 25, weight: .heavy))
+                    }
+                    .foregroundColor(.black)
+                
+                if showText {
+                    Text("[Paw Patrol is cool 🐾](https://www.youtube.com/watch?v=1UdI_eoDPKQ)")
+                        .padding(10)
+
+                }
+                    
+                
+            }
+        }
     }
 }
 
-struct Rachel_Previews: PreviewProvider {
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        Rachel()
+        ContentView()
     }
+}
 }

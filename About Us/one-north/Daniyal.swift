9c10312d700adeaf674417966ccbfa7b281b4a39
DWizard11 <daniyalisawesome111@gmail.com>
2022-06-18 12:03:59 +0800

    Daniyal About Me v1.0.1

--- About Us/one-north/Daniyal.swift
+++ About Us/one-north/Daniyal.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct Daniyal: View {
+    
+    @State var daniyalRotation = Angle(degrees: 60)
+    @State var aboutMeRotation = Angle(degrees: 60)
+    @State var daniyalScaleEffect = 0.1
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            LinearGradient(colors: [.red,.blue], startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("Daniyal")
+                    .padding(25)
+                    .font(.system(size: 50))
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    .cornerRadius(20)
+                    .scaleEffect(daniyalScaleEffect)
+                    .rotation3DEffect(daniyalRotation, axis: (1, 2, 3))
+                Text("About me, I am Daniyal")
+                    .padding()
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .background(.red)
+                    .cornerRadius(20)
+                    .rotation3DEffect(aboutMeRotation, axis: (0, 3, 0))
+            }
+            .onAppear {
+                withAnimation(.easeInOut.repeatForever()) {
+                    daniyalRotation = Angle(degrees: -30)
+                    aboutMeRotation = Angle(degrees: -30)
+                    daniyalScaleEffect = 1
+                    
+                }
+            }
+
+        }
     }
 }
 

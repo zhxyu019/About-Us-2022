761f62231a4a82fb0f42121fbcbf7e3ab07b71f4
Zhiyu <zhiyuma2006@gmail.com>
2022-06-18 12:16:00 +0800

    namecard praj.

--- About Us/one-north/Zhiyu.swift
+++ About Us/one-north/Zhiyu.swift
@@ -8,8 +8,76 @@
 import SwiftUI
 
 struct Zhiyu: View {
+    
+    @Environment(\.openURL) var openURL
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color
+                .red
+                .edgesIgnoringSafeArea(.all)
+            
+            Text("I am Zhiyu.")
+                .font(.system(size: 60, weight: .bold, design: .serif))
+                .italic()
+                .offset(x: 0, y: -350)
+                .foregroundColor(.brown)
+            
+            Text("I like to ruin people's eyes.")
+                .padding()
+                .font(.system(size:40, weight: .heavy))
+                .foregroundColor(.blue)
+                .offset(x:20, y: -370)
+                .rotation3DEffect(Angle(degrees: 20), axis: (x: 2, y: 1, z: 0))
+                
+            Text("and in the process ruin my perfect vision too!")
+                .padding()
+                .font(.system(size:20, weight: .thin))
+                .foregroundColor(.green)
+                .offset(x:0,  y: -150)
+                .rotation3DEffect(Angle(degrees: -30), axis: (x: 2, y: 1, z: 0))
+            
+            Text("I like red things, as you can see.")
+                .padding()
+                .offset(x:-50,  y: -100)
+
+            HStack {
+                Image("rickroll")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 180, height: 180 )
+                    .offset(x:0, y: -10)
+                
+               Image("doggie")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 180, height: 180 )
+                    .offset(x:0, y: 10)
+            }
+            
+            Image("paw patrol")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 180 )
+                .offset(x:0, y: 200)
+
+            Button(action: {
+                openURL(URL(string: "https://youtube.com/watch?v=dQw4w9WgXcQ")!)
+            }, label: {
+                HStack {
+                    Text("click for wholesome content.")
+                        .font(.system(size: 20, weight: .regular, design: .serif))
+                        .foregroundColor(.white)
+                    
+                }
+                .padding()
+                .background(Color.green.shadow(color: .red, radius: 3, x: 0.5, y: 1))
+            })
+            .rotationEffect(Angle(degrees: 0))
+            .offset(x:0, y: 350)
+            
+        
+        }
     }
 }
 
@@ -18,3 +86,4 @@ struct Zhiyu_Previews: PreviewProvider {
         Zhiyu()
     }
 }
+

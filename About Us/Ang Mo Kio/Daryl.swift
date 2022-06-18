c995c4c6034d4f0a72a11cd1381a146fa1894ce7
Daryl <daryllim3bro@gmail.com>
2022-06-18 11:56:14 +0800

    Done.

--- About Us/Ang Mo Kio/Daryl.swift
+++ About Us/Ang Mo Kio/Daryl.swift
@@ -8,8 +8,54 @@
 import SwiftUI
 
 struct Daryl: View {
+    @State var lineLength = 0.0
+    
+    func timer(){
+        lineLength = 0
+        withAnimation(Animation.easeInOut(duration: 60)){
+            lineLength = 1
+        }
+
+        
+        Timer.scheduledTimer(withTimeInterval: 60, repeats: true) { timer in
+            sleep(5)
+            lineLength = 0
+            withAnimation(Animation.easeInOut(duration: 60)){
+                lineLength = 1
+            }
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Path { path in
+                path.move(to: CGPoint(x: 0, y: 0))
+                for _ in 1 ... 1500 {
+                    let x = Int.random(in: 0...400)
+                    let y = Int.random(in: 0...750)
+                    withAnimation{
+                        path.addLine(to: CGPoint(x: x, y: y))
+                    }
+                }
+                    path.closeSubpath()
+            }
+            .trim(from: lineLength * 2 - 1 , to: lineLength)
+            .stroke(.cyan, style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round))
+            .task {
+                timer()
+            }
+            
+            VStack{
+                Text("Daryl")
+                    .font(.largeTitle)
+                    .padding()
+                Text("The line descibes Daryl's life")
+                    .font(.title)
+                    .padding()
+                Text("Completly random, with no fixed points except his birth and his inevitable, slow death.")
+                    .font(.title2)
+                    .padding()
+            }
     }
 }
 

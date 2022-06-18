7b0954587c5f4293779bc2d1e3b21593ab0892fd
nicoletanyt <nicole_tan_yitong@s2021.ssts.edu.sg>
2022-06-18 11:40:08 +0800

    added more text and button

--- About Us/Ang Mo Kio/Nicole.swift
+++ About Us/Ang Mo Kio/Nicole.swift
@@ -9,14 +9,35 @@ import SwiftUI
 
 struct Nicole: View {
     
-    @State var helloRotation: Angle = .zero
+    @State var helloRotation = Angle(degrees: 40)
+    
+    @Environment(\.openURL) var openURL
     
     var body: some View {
         
         VStack {
             Text("hello i'm")
-                .rotation3DEffect(helloRotation, axis: (0, 1, 1))
+                .font(.system(size: 40, weight: .bold, design: .serif))
+                .offset(y: -200)
             Text("nicole")
+                .rotation3DEffect(helloRotation, axis: (1, 0, 0))
+                .font(.system(size: 30, weight: .regular, design: .rounded))
+            Button{
+                openURL(URL(string: "https://youtu.be/a3LLv53LEuY")!)
+            }label: {
+                HStack{
+                    Image(systemName: "video.fill")
+                        .foregroundColor(.white)
+                        .font(.system(size: 20))
+                    Text("Watch this video")
+                        .foregroundColor(.white)
+                        .font(.system(size: 20, weight: .semibold, design: .serif))
+                }
+                .padding()
+            }
+            .background(Color.teal)
+            .cornerRadius(15)
+                
         }
     }
 }
